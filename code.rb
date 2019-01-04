d1ee7b622a56ac7b34065d77232ac204dfcc3431
Meder <m.toktosunov@icloud.com>
2019-01-03 22:29:24 -0500

    completed

--- code.rb
+++ code.rb
@@ -4,3 +4,10 @@ def greet(name)
 end
 
 puts greet('Zim')
+
+def greeting(name,language)
+salutation = {english: 'Hi', spanish: 'Hola', italian: 'Chao', irken: "DOOM de doom"}
+  "#{salutation[language.downcase.to_sym]}, #{name}"
+end
+
+puts greeting('Zim','Irken')
